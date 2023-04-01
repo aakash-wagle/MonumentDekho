1177eb23edd3b5ce25f78d533af18144b51d8c81
unknown <aak.wagle@gmail.com>
2023-04-02 04:19:36 +0530

    Updating App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Auth from "./Components/Auth/Auth";
-// import Landing from "./Components/Landing/Landing";
+import Landing from "./Components/Landing/Landing";
 import Completion from "./Components/Payment/Completion";
 import Payment from "./Components/Payment/Payment";
+import SignUp from "./Components/Auth/SignUp";
+import SignIn from "./Components/Auth/SIgnIn";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Auth />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup" exact element={<SignUp />} />
+          <Route path="/login" exact element={<SignIn />} />
           <Route path="/payment" exact element={<Payment />} />
           <Route path="/payment-complete" exact element={<Completion />} />
         </Routes>
